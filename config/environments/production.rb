3c89de44399f8c84a3105fdc74e11cab323b3a61
Alex Meyer <acmeyer9@gmail.com>
2015-12-16 20:16:14 -0500

    more removal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.middleware.use Rack::Deflater
-  config.middleware.use Rack::CanonicalHost
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.digest = true
   config.log_level = :debug
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {}
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
