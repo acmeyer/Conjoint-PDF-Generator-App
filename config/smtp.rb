1b9791a71167949215730de2caf2120803fb8fc9
Alex Meyer <acmeyer9@gmail.com>
2015-12-16 20:11:09 -0500

    remove more

--- config/smtp.rb
+++
@@ -1,9 +0,0 @@
-SMTP_SETTINGS = {
-  address: 'smtp.example.com',
-  authentication: :plain,
-  domain: 'example.com',
-  enable_starttls_auto: true,
-  password: 'password',
-  port: "587",
-  user_name: 'username'
-}
